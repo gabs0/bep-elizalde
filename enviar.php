b313b9559143aad734d79d952e3f8ff6047bedca
desarrollo00 <gabrielaelizalde96@gmail.com>
2022-01-24 20:21:28 -0300

    arreglos en slider servicios, archivo php

---
+++ enviar.php
@@ -0,0 +1,14 @@
+<?php
+$destino = "gabrielaelizalde96@gmail.com";
+$nombre = $_POST["nombre"];
+$asunto = $_POST["asunto"];
+$correo = $_POST["correo"];
+$consulta = $_POST["consulta"];
+$comentarios = $_POST["comentarios"];
+
+$contenido = "Nombre: "  . $nombre . "\nAsunto: "  . $asunto ."\nCorreo: " .$correo. "\nConsulta: " . $consulta ;
+    
+    
+mail($destino,"contacto",$contenido);
+
+?>
--- glider/app.js
+++ glider/app.js
@@ -1,13 +1,47 @@
 window.addEventListener("load", function () {
     new Glider(document.querySelector(".carousel__servicios"), {
-        slidesToShow: 4,
+        slidesToShow: 1,
         slidesToScroll: 1,
         draggable: true,
+        itemWidth: 550,
         dots: ".carousel__indicadores",
         arrows: {
             prev: ".carousel__prev",
             next: ".carousel__next",
         },
-        
+        responsive: [
+            {
+                // screens greater than >= 775px
+                breakpoint: 680,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 2,
+                    slidesToScroll:1 ,
+                    itemWidth: 350,
+                    duration: 0.25
+                }
+            }, 
+            {
+                // screens greater than >= 1000px
+                breakpoint: 970,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 3,
+                    slidesToScroll:1 ,
+                    itemWidth: 350,
+                    duration: 0.25
+                }
+            }, 
+            {
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    itemWidth: 350,
+                    duration: 0.25
+                }
+            }
+        ]
     });
 });
